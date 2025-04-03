34d9da6707973e76a9ece9332c11d15598908b5a
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2025-04-03 09:42:20 +0200

    Add comments to the file

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,16 +1,19 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';//import required package
+
 
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  // Fetch learning modules
+  // Fetch necessary learning modules
   Future<List<QueryDocumentSnapshot>> getLearningModules() async {
     QuerySnapshot snapshot = await _db.collection('learningModules').get();
     return snapshot.docs;
   }
 
-  // Fetch quizzes by module ID
+  // Fetch quizzes by  the module ID
   Future<DocumentSnapshot> getQuizById(String quizId) async {
     return await _db.collection('quizzes').doc(quizId).get();
+    
   }
+  
 }
